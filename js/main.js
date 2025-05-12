8aa80b2717beee791f248151c421e8834d16c651
Joaquin Noel <joaquinnoellll@gmail.com>
2025-05-12 14:47:29 -0300

    empeze con js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Declarion:
+let cliente;
+
+
+//Asignacion de Valores:
+cliente = "Carlos";
+
+
+console.log(cliente);
+
+
+
+//Inicializar Variables:
+let edad = 37
+console.log(edad);
+
+
+//Constante:
+
+const Nacimiento = 2004
+console.log(Nacimiento);
