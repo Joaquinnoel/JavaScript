727731672145198c904adbd58fe27648ee0da81c
Joaquin Noel <joaquinnoellll@gmail.com>
2025-05-13 20:27:29 -0300

    clase1

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@ let cliente;
 
 //Asignacion de Valores:
 cliente = "Carlos";
+console.log (cliente)
 
+cliente = "Roman";
 
 console.log(cliente);
 
 
 
+
 //Inicializar Variables:
-let edad = 37
+const edad = 37
 console.log(edad);
 
 
@@ -19,3 +22,18 @@ console.log(edad);
 
 const Nacimiento = 2004
 console.log(Nacimiento);
+
+
+
+// Numeros
+
+const cumple = 1
+
+const actual = ( edad + cumple)
+
+console.log (actual)
+
+
+const RespuestaUsuario = prompt ("Cual es tu nombre?")
+
+console.log(RespuestaUsuario);
