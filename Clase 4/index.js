7ede63f4773998f8358200b646bdd75e686729f3
Joaquin Noel <joaquinnoellll@gmail.com>
2025-06-03 09:38:13 -0300

    Clase 4

---
+++ Clase 4/index.js
@@ -0,0 +1,23 @@
+let numeros = [1,2,3,4,5];
+
+console.log ( numeros);
+
+console.log (numeros[2]);
+
+
+let palabras = ["auto","manzana", "pera", "casa"];
+console.log (palabras);
+console.log (palabras[1]);
+
+let booleanos = [true, false, true];
+console.log (booleanos);
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+
+console.log(matriz);
+console.log(matriz[2][1]); // Acceso al elemento 8 en la matriz
